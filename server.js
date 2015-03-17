ab7021c389214993d9b2bfa1dc15c355d420dfd4
Jacob Barnett <mrjbarnett@gmail.com>
2015-03-17 12:02:59 -0700

    server and basic routes up, need to modularize

---
+++ server.js
@@ -0,0 +1,55 @@
+var mongoose = require('mongoose');
+var express = require('express');
+var app = express();
+var User = require('./models/user_model');
+var Song = require('./models/song_model');
+var bodyparser = require('body-parser');
+
+mongoose.connect(process.env.MONG_URI || 'mongodb://localhost/muder_music');
+
+app.use(bodyparser.json());
+
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+//returns array of all users
+app.get('/user', function(req, res) {
+  User.find()
+  .exec(function(err, data) {
+    if (err) res.status(500).send({msg: 'could not get user'});
+    res.json(data);
+  });
+});
+
+//creates new user in database
+app.post('/user', function(req, res) {
+  var newUser = new User(req.body);
+  newUser.save(function(err, data) {
+    if (err) res.status(500).send({msg: 'could not save user'});
+    res.json(data);
+  });
+});
+
+//creates a song
+app.post('/song', function(req, res) {
+  var newSong = new Song(req.body);
+  newSong.save(function(err, data) {
+    if (err) res.status(500).send({msg: 'could not save song'});
+    res.json(data);
+  });
+});
+
+//gets all songs posted by a user
+app.get('/userSongs/:userId', function(req, res) {
+  Song.find()
+  .where('postedBy').equals(req.params.userId)
+  .exec(function(err, data) {
+    if (err) console.log(err);
+    res.json(data);
+  });
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Server Listening port');
+});
